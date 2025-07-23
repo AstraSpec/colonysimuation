ebfef5198baef55957c9935302b9a022cea939ba
AstraSpec <rizvan.faisal@gmail.com>
2025-07-23 22:39:46 +0100

    Adds world generation

--- colonysimulation/src/main.gd
+++ colonysimulation/src/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var WorldGeneration :FastTileMap
+@export var WorldGeneration :Node2D
 
 func _ready() -> void:
 	WorldGeneration.generate_world()
--- colonysimulation/src/world_generation.gd
+++ colonysimulation/src/world_generation.gd
@@ -1,10 +1,31 @@
-extends FastTileMap
+extends Node2D
 
-@onready var Tiles :CompressedTexture2D = preload("res://assets/tiles.png")
+@onready var MapNoise :FastNoiseLite = preload("res://assets/noise/map.tres")
+@export var WallLayer :TileMapLayer
+@export var TerrainLayer :TileMapLayer
 
-const WORLD_SIZE :int = 256
+const WORLD_SIZE :int = 100
 const TILE_SIZE :int = 16
 
 func generate_world() -> void:
+	WallLayer.clear()
+	TerrainLayer.clear()
+	
+	var wallsPos :Array = []
+	for i in WORLD_SIZE ** 2:
+		var cellPos := Vector2i(i / WORLD_SIZE, i % WORLD_SIZE)
+		
+		var noise = MapNoise.get_noise_2d(cellPos.x, cellPos.y)
+		if noise > 0.3:
+			wallsPos.append(cellPos)
+	
+	var dirtTerrain :Dictionary = TilesDb.get_data("dirt_terrain")
+	var dirtAtlas :Vector2i = Vector2(dirtTerrain["atlas"][0], dirtTerrain["atlas"][1])
+	
+	for x in WORLD_SIZE / 3:
+		for y in WORLD_SIZE / 3:
+			TerrainLayer.set_cell(Vector2(x * 3, y * 3), 0, dirtAtlas)
+	
 	var stoneWall :Dictionary = TilesDb.get_data("stone_wall")
-	set_cells_autotile([Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0),Vector2(1, 1), Vector2(1, 2), Vector2(1, -1)], stoneWall["atlas"], TILE_SIZE, Tiles)
+	WallLayer.set_cells_terrain_connect(wallsPos, 0, stoneWall["terrain"])
+	
