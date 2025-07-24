296333922045b973e4c29f46882c6374ffc2e7b1
AstraSpec <rizvan.faisal@gmail.com>
2025-07-24 21:21:46 +0100

    Changes camera zooming

--- colonysimulation/src/camera.gd
+++ colonysimulation/src/camera.gd
@@ -1,30 +1,37 @@
 extends Camera2D
 
-var drag_speed :float = 1.75
-
-var zoom_increment :float = 0.25
-var zoom_min :float = 0.5
-var zoom_max :float = 5
-
-const MOUSE_WHEEL_UP :int = 4
-const MOUSE_WHEEL_DOWN :int = 5
-
+const DRAG_SPEED :float = 1.75
 const MOVE_SPEED :float = 1000.0
+
+const ZOOM_LEVELS :Array = [0.25, 0.33, 0.5, 0.66, 1.0, 2.0]
+
+var zoomLevel :int = 2
+var scrolled :bool = false
 var dir :Vector2
 
 func _unhandled_input(event :InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		position -= event.relative * drag_speed / zoom
+		position -= event.relative * DRAG_SPEED / zoom
+	
+	elif scrolled:
+		return
 	
 	elif event is InputEventMouseButton:
-		if event.button_index == MOUSE_WHEEL_UP:
-			zoom += Vector2(zoom_increment, zoom_increment)
-		elif event.button_index == MOUSE_WHEEL_DOWN:
-			zoom -= Vector2(zoom_increment, zoom_increment)
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			change_zoom(1)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			change_zoom(-1)
+
+func change_zoom(amount :int) -> void:
+	zoomLevel = clamp(zoomLevel + amount, 0, ZOOM_LEVELS.size() - 1)
+	zoom = Vector2(ZOOM_LEVELS[zoomLevel], ZOOM_LEVELS[zoomLevel])
 	
-		zoom = zoom.clamp(Vector2(zoom_min, zoom_min), Vector2(zoom_max, zoom_max))
+	scrolled = true
+	print(ZOOM_LEVELS[zoomLevel])
 
 func _process(delta :float) -> void:
+	scrolled = false
+	
 	dir.x = Input.get_axis("a", "d")
 	dir.y = Input.get_axis("w", "s")
 	
