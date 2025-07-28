b666dabd2202e4537e84c0942b6a5c0a5861d8c1
AstraSpec <rizvan.faisal@gmail.com>
2025-07-28 23:02:42 +0100

    Adds grass variants

--- colonysimulation/src/world_generation.gd
+++ colonysimulation/src/world_generation.gd
@@ -25,7 +25,10 @@ func generate_world() -> void:
 	var tree2 :Dictionary = TilesDb.get_config("tree2")
 	var tree3 :Dictionary = TilesDb.get_config("tree3")
 	var grass :Dictionary = TilesDb.get_config("grass")
-	var tallGrass :Dictionary = TilesDb.get_config("tall_grass")
+	var tallGrass1 :Dictionary = TilesDb.get_config("tall_grass1")
+	var tallGrass2 :Dictionary = TilesDb.get_config("tall_grass2")
+	var tallGrass3 :Dictionary = TilesDb.get_config("tall_grass3")
+	var tallGrass4 :Dictionary = TilesDb.get_config("tall_grass4")
 	
 	# Initialize arrays for each tile type
 	terrainPos[dirtTerrain] = []
@@ -34,7 +37,10 @@ func generate_world() -> void:
 	terrainPos[tree1] = []
 	terrainPos[tree2] = []
 	terrainPos[tree3] = []
-	terrainPos[tallGrass] = []
+	terrainPos[tallGrass1] = []
+	terrainPos[tallGrass2] = []
+	terrainPos[tallGrass3] = []
+	terrainPos[tallGrass4] = []
 	autotilePos[stoneWall] = []
 	autotilePos[mudWall] = []
 	grassPos[grass] = []
@@ -59,8 +65,17 @@ func generate_world() -> void:
 			if GrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.4:
 				grassPos[grass].append(cellPos)
 			
-			if TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.6:
-				terrainPos[tallGrass].append(cellPos)
+			if TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.555:
+				if randf() > 0.05:
+					terrainPos[tallGrass2].append(cellPos)
+				else:
+					if randf() > 0.5:
+						terrainPos[tallGrass3].append(cellPos)
+					else:
+						terrainPos[tallGrass4].append(cellPos)
+			elif TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.45:
+				terrainPos[tallGrass1].append(cellPos)
+			
 	
 	for t in terrainPos:
 		Tilemap.set_cells(terrainPos[t], t)
