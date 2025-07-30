8f6611624a24b11732c27ce74341c8936f0c4365
AstraSpec <rizvan.faisal@gmail.com>
2025-07-30 19:38:26 +0100

    Utilizes noise constants

--- colonysimulation/src/world_generation.gd
+++ colonysimulation/src/world_generation.gd
@@ -11,6 +11,14 @@ extends Node2D
 const WORLD_SIZE :int = 250
 const TILE_SIZE :int = 16
 
+const TERRAIN_THRESHOLD :float = 0.27
+const CLIFF_THRESHOLD :float = 0.3
+const GRASS_THRESHOLD :float = 0.4
+const TREE_THRESHOLD :float = 0.25
+const TALL_GRASS_HIGH_THRESHOLD :float = 0.555
+const TALL_GRASS_LOW_THRESHOLD :float = 0.45
+const FLOWER_THRESHOLD :float = 0.05
+
 func generate_world() -> void:
 	Tilemap.clear_cells()
 	
@@ -25,15 +33,17 @@ func generate_world() -> void:
 		process_tile(cellPos, tilePos)
 	
 	for tile in terrainPos: Tilemap.set_terrain_cells(terrainPos[tile], TilesDb.get_config(tile))
-	for tile in floorPos: Tilemap.set_cells_autotile(floorPos[tile], TilesDb.get_config(tile), floorPos[tile])
-	for tile in objectPos: Tilemap.set_cells(objectPos[tile], TilesDb.get_config(tile))
+	for tile in floorPos:
+		Tilemap.set_cells_autotile(floorPos[tile], TilesDb.get_config(tile), floorPos[tile])
+	for tile in objectPos: 
+		Tilemap.set_cells(objectPos[tile], TilesDb.get_config(tile))
 	
 	var totalPos :Array = []
 	for tile in wallPos: totalPos.append_array(wallPos[tile])
 	for tile in wallPos: Tilemap.set_cells_autotile(wallPos[tile], TilesDb.get_config(tile), totalPos)
 	
 func process_tile(cellPos :Vector2, tilePos :Dictionary) -> void:
-	var noise = {
+	var noise :Dictionary = {
 		"elevation": ElevationNoise.get_noise_2d(cellPos.x, cellPos.y),
 		"geology": GeologyNoise.get_noise_2d(cellPos.x, cellPos.y),
 		"grass": GrassNoise.get_noise_2d(cellPos.x, cellPos.y),
@@ -42,47 +52,44 @@ func process_tile(cellPos :Vector2, tilePos :Dictionary) -> void:
 	}
 	
 	process_terrain(cellPos, tilePos["terrain"], noise)
+	process_wall(cellPos, tilePos["wall"], noise)
 	
-	var has_wall = process_wall(cellPos, tilePos["wall"], noise)
-	
-	if not has_wall:
-		process_vegetation(cellPos, tilePos, noise)
+	if noise.elevation <= TERRAIN_THRESHOLD:
+		process_floor(cellPos, tilePos["floor"], noise)
+		process_object(cellPos, tilePos["object"], noise)
 
 func process_terrain(cellPos :Vector2, terrainPos :Dictionary, noise :Dictionary) -> void:
-	if noise.elevation > 0.27 && noise.geology > 0.0:
+	if noise.elevation > TERRAIN_THRESHOLD && noise.geology > 0.0:
 		terrainPos["stone_terrain"].append(cellPos)
 	else:
 		terrainPos["dirt_terrain"].append(cellPos)
 
-func process_wall(cellPos :Vector2, wallPos :Dictionary, noise :Dictionary) -> bool:
-	if noise.elevation > 0.3:
-		if noise.geology > 0.0:
-			wallPos["stone_wall"].append(cellPos)
-		else:
-			wallPos["mud_wall"].append(cellPos)
-		
-		return true
-	return false
+func process_wall(cellPos :Vector2, wallPos :Dictionary, noise :Dictionary) -> void:
+	if noise.elevation > CLIFF_THRESHOLD:
+		var type :String = "stone_wall" if noise.geology > 0.0 else "mud_wall"
+		wallPos[type].append(cellPos)
 
-func process_vegetation(cellPos :Vector2, tilePos :Dictionary, noise :Dictionary) -> void:
-	var floorPos = tilePos["floor"]
-	var objectPos = tilePos["object"]
-	
-	if noise.grass > 0.4:
+func process_floor(cellPos :Vector2, floorPos :Dictionary, noise :Dictionary) -> void:
+	if noise.grass > GRASS_THRESHOLD:
 		floorPos["grass"].append(cellPos)
-			
-	if noise.tree > 0.25:
-		if randf() > 0.33: objectPos["tree1"].append(cellPos)
-		elif randf() > 0.5: objectPos["tree2"].append(cellPos)
-		else: objectPos["tree3"].append(cellPos)
+
+func process_object(cellPos :Vector2, objectPos :Dictionary, noise :Dictionary) -> void:
+	var vegetation :String = get_vegetation(cellPos, objectPos, noise)
+	if vegetation != "":
+		objectPos[vegetation].append(cellPos)
+
+func get_vegetation(cellPos :Vector2, objectPos :Dictionary, noise :Dictionary) -> String:
+	if noise.tree > TREE_THRESHOLD:
+		var trees :Array = ["tree1", "tree2", "tree3"]
+		trees.shuffle()
+		return trees[0]
 	
-	elif noise.tallGrass > 0.555:
-		if randf() > 0.05:
-			objectPos["tall_grass2"].append(cellPos)
+	elif noise.tallGrass > TALL_GRASS_HIGH_THRESHOLD:
+		if randf() > FLOWER_THRESHOLD:
+			return "tall_grass2"
 		else:
-			if randf() > 0.5:
-				objectPos["tall_grass3"].append(cellPos)
-			else:
-				objectPos["tall_grass4"].append(cellPos)
-	elif noise.tallGrass > 0.45:
-		objectPos["tall_grass1"].append(cellPos)
+			return "flower1" if randf() > 0.5 else "flower2"
+	elif noise.tallGrass > TALL_GRASS_LOW_THRESHOLD:
+		return "tall_grass1"
+	
+	return ""
