d3b2d49a2b7255f5b601f5bdae1ac290b7548a88
AstraSpec <rizvan.faisal@gmail.com>
2025-07-30 16:58:56 +0100

    Cleans up tile generation code

--- colonysimulation/src/world_generation.gd
+++ colonysimulation/src/world_generation.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@onready var MapNoise :FastNoiseLite = preload("res://assets/noise/map.tres")
+@onready var ElevationNoise :FastNoiseLite = preload("res://assets/noise/elevation.tres")
+@onready var GeologyNoise :FastNoiseLite = preload("res://assets/noise/geology.tres")
 @onready var TreeNoise :FastNoiseLite = preload("res://assets/noise/trees.tres")
 @onready var GrassNoise :FastNoiseLite = preload("res://assets/noise/grass.tres")
 @onready var TallGrassNoise :FastNoiseLite = preload("res://assets/noise/tall_grass.tres")
-@onready var WallNoise :FastNoiseLite = preload("res://assets/noise/walls.tres")
 
 @export var Tilemap :FastTileMap
 
@@ -22,45 +22,7 @@ func generate_world() -> void:
 	
 	for i in WORLD_SIZE ** 2:
 		var cellPos := Vector2i(i / WORLD_SIZE, i % WORLD_SIZE)
-		
-		var map_noise = MapNoise.get_noise_2d(cellPos.x, cellPos.y)
-		var wall_noise = WallNoise.get_noise_2d(cellPos.x, cellPos.y)
-		var grass_noise = GrassNoise.get_noise_2d(cellPos.x, cellPos.y)
-		var tree_noise = TreeNoise.get_noise_2d(cellPos.x, cellPos.y)
-		var tall_grass_noise = TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y)
-		
-		if MapNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.27:
-			if WallNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.0:
-				terrainPos["stone_terrain"].append(cellPos)
-			else:
-				terrainPos["dirt_terrain"].append(cellPos)
-		else:
-			terrainPos["dirt_terrain"].append(cellPos)
-		
-		if MapNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.3:
-			if WallNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.0:
-				wallPos["stone_wall"].append(cellPos)
-			else:
-				wallPos["mud_wall"].append(cellPos)
-		else:
-			if GrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.4:
-				floorPos["grass"].append(cellPos)
-			
-			if TreeNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.25:
-				if randf() > 0.33: objectPos["tree1"].append(cellPos)
-				elif randf() > 0.5: objectPos["tree2"].append(cellPos)
-				else: objectPos["tree3"].append(cellPos)
-			
-			elif TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.555:
-				if randf() > 0.05:
-					objectPos["tall_grass2"].append(cellPos)
-				else:
-					if randf() > 0.5:
-						objectPos["tall_grass3"].append(cellPos)
-					else:
-						objectPos["tall_grass4"].append(cellPos)
-			elif TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y) > 0.45:
-				objectPos["tall_grass1"].append(cellPos)
+		process_tile(cellPos, tilePos)
 	
 	for tile in terrainPos: Tilemap.set_terrain_cells(terrainPos[tile], TilesDb.get_config(tile))
 	for tile in floorPos: Tilemap.set_cells_autotile(floorPos[tile], TilesDb.get_config(tile), floorPos[tile])
@@ -70,4 +32,57 @@ func generate_world() -> void:
 	for tile in wallPos: totalPos.append_array(wallPos[tile])
 	for tile in wallPos: Tilemap.set_cells_autotile(wallPos[tile], TilesDb.get_config(tile), totalPos)
 	
+func process_tile(cellPos :Vector2, tilePos :Dictionary) -> void:
+	var noise = {
+		"elevation": ElevationNoise.get_noise_2d(cellPos.x, cellPos.y),
+		"geology": GeologyNoise.get_noise_2d(cellPos.x, cellPos.y),
+		"grass": GrassNoise.get_noise_2d(cellPos.x, cellPos.y),
+		"tallGrass": TallGrassNoise.get_noise_2d(cellPos.x, cellPos.y),
+		"tree": TreeNoise.get_noise_2d(cellPos.x, cellPos.y),
+	}
 	
+	process_terrain(cellPos, tilePos["terrain"], noise)
+	
+	var has_wall = process_wall(cellPos, tilePos["wall"], noise)
+	
+	if not has_wall:
+		process_vegetation(cellPos, tilePos, noise)
+
+func process_terrain(cellPos :Vector2, terrainPos :Dictionary, noise :Dictionary) -> void:
+	if noise.elevation > 0.27 && noise.geology > 0.0:
+		terrainPos["stone_terrain"].append(cellPos)
+	else:
+		terrainPos["dirt_terrain"].append(cellPos)
+
+func process_wall(cellPos :Vector2, wallPos :Dictionary, noise :Dictionary) -> bool:
+	if noise.elevation > 0.3:
+		if noise.geology > 0.0:
+			wallPos["stone_wall"].append(cellPos)
+		else:
+			wallPos["mud_wall"].append(cellPos)
+		
+		return true
+	return false
+
+func process_vegetation(cellPos :Vector2, tilePos :Dictionary, noise :Dictionary) -> void:
+	var floorPos = tilePos["floor"]
+	var objectPos = tilePos["object"]
+	
+	if noise.grass > 0.4:
+		floorPos["grass"].append(cellPos)
+			
+	if noise.tree > 0.25:
+		if randf() > 0.33: objectPos["tree1"].append(cellPos)
+		elif randf() > 0.5: objectPos["tree2"].append(cellPos)
+		else: objectPos["tree3"].append(cellPos)
+	
+	elif noise.tallGrass > 0.555:
+		if randf() > 0.05:
+			objectPos["tall_grass2"].append(cellPos)
+		else:
+			if randf() > 0.5:
+				objectPos["tall_grass3"].append(cellPos)
+			else:
+				objectPos["tall_grass4"].append(cellPos)
+	elif noise.tallGrass > 0.45:
+		objectPos["tall_grass1"].append(cellPos)
